1eb9f5493a5147aa95bff5f6c1c54733e5b5562a
Dan Obermiller <dannnno16@gmail.com>
2015-03-25 22:28:58 -0700

    Example pages

---
+++ Getting_Started/first_program.py
@@ -0,0 +1,6 @@
+def celsius_to_fahrenheit(celsius_temp):
+    fahrenheit_temp = (9.0 / 5.0) * celsius_temp + 32
+    return fahrenheit_temp
+
+print(celsius_to_fahrenheit(100))
+print(celsius_to_fahrenheit(0))
